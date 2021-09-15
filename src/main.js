93b467b9dfef72eae911e002d8ae30b5beaaa1ad
Larisa Frolova <larisa@frolova.org>
2021-09-15 12:33:21 -0700

    NewRecipe.vue component was imported to the App.vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import NewRecipe from './components/NewRecipe.vue';
 
 const app = createApp(App);
+
+app.component ('new-recipe', NewRecipe);
+
 app.mount('#app');
