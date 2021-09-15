f9eed22aedb03a74906e83d2234f504361042a93
Larisa Frolova <larisa@frolova.org>
2021-09-15 12:31:57 -0700

    a form with label and inputs is added

---
+++ src/components/NewRecipe.vue
@@ -0,0 +1,40 @@
+<template>
+    <form>
+        <div>
+            <label>Title</label>
+            <input v-model="userTitle" type="text">
+        </div>
+        <div>
+            <label>Course</label>
+            <input v-model="userCourse" type="text"/>
+        </div>
+        <div>
+            <label>Ingredients</label>
+            <textarea v-model="userIngredients" rows="30" cols="30"></textarea>
+        </div>
+        <div>
+            <label>Directions</label>
+            <textarea v-model="userDirections" rows="30" cols="30"></textarea>
+        </div>
+        
+    </form>
+</template>
+
+<script>
+export default {
+    data (){
+        return {
+            userTitle: '',
+            userCourse: '',
+            userIngredients: '',
+            userDirections: ''
+        };
+    }
+};
+
+
+</script>
+
+<style>
+
+</style>
